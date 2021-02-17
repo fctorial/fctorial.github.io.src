f88cc841488173b7d73086e3dd24ac3e56c0f82b
Sagar Tiwari <fctorial@gmail.com>
2021-02-17 10:49:01 +0530

    404 and build setup

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-constexpr.js --input=. --output=../fctorial.github.io --jobs 5 --exclusions=/collections:/_template.html --force
+rm -rf ../fctorial.github.io/*
+node ../ConstexprJS/constexpr.js --input=. --output=../fctorial.github.io --jobs 5 --exclusions=/demos:/collections:/_template.html --force "$@"
+cp -r demos ../fctorial.github.io
