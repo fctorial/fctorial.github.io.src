21337f4971a3f4ed2023bf9c8269b7865024e3a0
Sagar Tiwari <fctorial@gmail.com>
2021-02-23 07:39:39 +0530

    copy favicon

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 rm -rf ../fctorial.github.io/*
-constexpr.js "$@" --input=. --output=../fctorial.github.io --jobs 5 --exclusions=/demos:/collections:/_template.html --force --verbose
-cp -r demos ../fctorial.github.io
-cp serve.sh ../fctorial.github.io
+constexpr.js "$@" --input=. --output=../fctorial.github.io --jobs 5 --exclusions=/demos:/collections:/_template.html:/static/js/constexpr --force --verbose
+cp -r demos favicon.png ../fctorial.github.io
