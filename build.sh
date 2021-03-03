0c93357336254c986f9bd452d5c397e15cee4b2d
Sagar Tiwari <fctorial@gmail.com>
2021-03-03 10:23:59 +0530

    update

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 rm -rf ../fctorial.github.io/*
 constexpr.js "$@" --input=. --output=../fctorial.github.io --jobs 5 --exclusions=/demos:/collections:/static/js/constexpr/third_party --force --verbose
-cp -r demos favicon.png ../fctorial.github.io
+#cp -r demos favicon.png ../fctorial.github.io
