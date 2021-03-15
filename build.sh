8b6b8dcd42d29b998cb1ed4464bac0cd252da057
Sagar Tiwari <fctorial@gmail.com>
2021-03-15 20:42:06 +0530

    tags

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 rm -rf ../fctorial.github.io/*
-constexpr.js "$@" --input=. --output=../fctorial.github.io --jobs 5 --exclusions=/collections:/static/js/constexpr/third_party --force --verbose
+../ConstexprJS/constexpr.js "$@" --input=. --output=../fctorial.github.io --jobs 5 --exclusions=/collections:/tags/generator.html:/static/files:/static/js/constexpr/third_party --force --verbose
 cp -r favicon.ico ../fctorial.github.io
