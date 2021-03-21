4ec9a755531da9a1ebdc40b8eb85cda4efc16ce2
Sagar Tiwari <fctorial@gmail.com>
2021-03-21 23:13:04 +0530

    build

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 rm -rf ../fctorial.github.io/*
-../ConstexprJS/constexpr.js "$@" --input=. --output=../fctorial.github.io --jobs 10 --exclusions=/collections:/tags/generator.html:/static/files --force --verbose
+../ConstexprJS/constexpr.js "$@" --input=. --output=../fctorial.github.io --jobs 10 --exclusions=/collections:/tags/generator.html:/static/files --force --verbose --depfile deps.json
