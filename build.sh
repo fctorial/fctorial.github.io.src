cda10a1f8f5951ee1c323505ffe477370d693b6c
Sagar Tiwari <fctorial@gmail.com>
2021-03-27 04:18:12 +0530

    link updates

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 rm -rf ../fctorial.github.io/*
-../ConstexprJS/constexpr.js "$@" --input=. --output=../fctorial.github.io --entry /index.html --jobcount 10 --exclusion /collections --depfile deps.json
+../ConstexprJS/constexpr.js "$@" --input=. --output=../fctorial.github.io --entry /index.html --jobcount 10 --exclusion /collections --exclusion /raw --depfile deps.json
+cp -r raw ../fctorial.github.io/raw
