c366d9ede50a9f2c2eed74e57bea5513c238709f
Sagar Tiwari <fctorial@gmail.com>
2021-04-12 03:42:12 +0530

    update

--- devtools/build.sh
+++ devtools/build.sh
@@ -1,5 +1,5 @@
 export OUT=../fctorial.github.io/
 rm -rf ${OUT:?}/*
 ./devtools/generate_rss.js &
-../ConstexprJS/constexpr.js "$@" --input=. --output=$OUT --entry /index.html --jobcount 10 --depfile devtools/deps.json
+constexpr.js "$@" --input=. --output=$OUT --entry /index.html --jobcount 10 --depfile devtools/deps.json
 cp -r raw ${OUT}/raw
