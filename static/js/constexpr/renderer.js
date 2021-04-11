58860db107bfcf32e2b6aaa38da1ff4b0005c982
Sagar Tiwari <fctorial@gmail.com>
2021-04-12 03:37:14 +0530

    dependency resolution

--- static/js/constexpr/renderer.js
+++ static/js/constexpr/renderer.js
@@ -53,12 +53,7 @@ async function render_base_page() {
   )
   document.head.appendChild(
     make_element(
-      `<link href="https://fonts.googleapis.com/css2?family=Ubuntu+Condensed&display=swap" rel="stylesheet">`
-    )
-  )
-  document.head.appendChild(
-    make_element(
-      `<link href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">`
+      `<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">`
     )
   )
   if (this_post) {
