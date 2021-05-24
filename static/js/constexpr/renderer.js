facf396a3aec8e88a31fef1c48cb72ba4afd5571
Sagar Tiwari <fctorial@gmail.com>
2021-05-24 15:17:45 +0530

    form ui

--- static/js/constexpr/renderer.js
+++ static/js/constexpr/renderer.js
@@ -280,10 +280,12 @@ async function site_global_rendering() {
     <div class="dialog">
         <div class="heading">Join the newsletter</div>
         <div class="content">
-          <form action="https://fctorial.com/apis/email/register" enctype="text/plain" method="post" target="_blank">
+          <form action="/apis/email/register" enctype="text/plain" method="post" target="_blank">
               <label for="subscriber_email">Your Email:</label>
               <input type="email" placeholder="your email" id="subscriber_email" name="subscriber_email" required>
               <input type="submit" value="Register">
+              <div class="message">
+              </div>
           </form>
         </div>
     </div>
@@ -298,13 +300,16 @@ async function site_global_rendering() {
       <noscript>
 <style>
   .open, .close {
-      display: none;
-  }   
+      /*display: none;*/
+  }
+  #right-sidebar .message {
+      /*display: none;*/
+  }
 </style>
       </noscript>
       `)
   )
-  function runtime_setup() {
+  function swiper_setup() {
     function addToBodyLeft(n) {
       let curr = document.body.style.left ? parseInt(document.body.style.left) : -100;
       document.body.style.left = `${curr + n}vw`
@@ -326,20 +331,57 @@ async function site_global_rendering() {
   }
 
   let el = document.createElement('script')
-  el.textContent = runtime_setup.toString()
+  el.textContent = swiper_setup.toString()
   document.body.appendChild(el)
 
   el = document.createElement('script')
-  el.textContent = 'runtime_setup()'
+  el.textContent = 'swiper_setup()'
   document.body.appendChild(el)
 
-  async function f() {
-    let n = 0;
-    while (true) {
-      await sleep(32);
-      n += 1
-      document.body.style.left = `${(Math.sin(n * Math.PI / 180) - 1) * 100}%`
-    }
+  function form_ui_setup() {
+    let toast = document.querySelector('#right-sidebar .message');
+    let box = document.querySelector('#right-sidebar input[type=email]')
+    document.querySelector('#right-sidebar form').addEventListener(
+      'submit',
+      e => {
+        e.preventDefault()
+        let body = `subscriber_email=${box.value}`
+
+        toast.textContent = 'Please wait...'
+        let req = new XMLHttpRequest();
+        req.onreadystatechange = function() {
+          if (this.readyState === 4) {
+            if (this.status !== 200) {
+              toast.textContent = `Network Error: ${this.status}`
+            } else if (this.responseText === 'INVALID_EMAIL') {
+              toast.textContent = 'The provided email address is not valid'
+            } else if (this.responseText === 'ALREADY_REGISTERED') {
+              toast.textContent = 'The provided email is already registered'
+            } else if (this.responseText === 'QUOTA_EXCEEDED') {
+              toast.textContent = 'You are doing that too much, please try again later'
+            } else if (this.responseText === 'BACKEND_ERROR') {
+              toast.textContent = 'Their is something wrong with the backend, please try again tomorrow'
+            } else if (this.responseText === 'EMAIL_NOT_SENT') {
+              toast.textContent = 'A verification email will be sent to your address in a while, please verify your email address when you recieve it'
+            } else if (this.responseText === 'OK') {
+              toast.textContent = 'A verification link has been sent to this address, please verify your email address'
+            } else {
+              toast.textContent = `Unknown error: ${this.responseText}`
+            }
+          }
+        };
+        req.open("POST", "/apis/email/register", true);
+        req.send(body);
+      }
+    )
   }
-  // f()
+
+  el = document.createElement('script')
+  el.textContent = form_ui_setup.toString()
+  document.body.appendChild(el)
+
+  el = document.createElement('script')
+  el.textContent = 'form_ui_setup()'
+  document.body.appendChild(el)
+
 }
