5d0856a483163387b0e83767d581f09da7859b73
Sagar Tiwari <fctorial@gmail.com>
2021-07-04 10:38:54 +0530

    fast forward

--- devtools/serve.sh
+++ devtools/serve.sh
@@ -1,5 +1,5 @@
 (sleep 1; google-chrome-stable --new-window http://localhost:1750)
 sass --watch static/scss/styles.scss:static/css/styles.css --style compressed&
 SASS_JOB=$!
-python -m http.server 1750
+http-server -p 1750
 kill -9 $SASS_JOB
